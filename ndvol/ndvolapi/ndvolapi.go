ef301a7a423949562761f87c98dce7081fef401b
Nabin Acharya <nabin.acharya@nexenta.com>
2016-07-14 13:23:07 -0700

    Update the Client to all the driver APIs

--- ndvol/ndvolapi/ndvolapi.go
+++ ndvol/ndvolapi/ndvolapi.go
@@ -4,38 +4,46 @@ import (
 	"encoding/json"
 	log "github.com/Sirupsen/logrus"
 	"io/ioutil"
+	"errors"
+	"net/http"
+	"strings"
+	"time"
 )
 
+var (
+	DN = "ndvolapi "
+)
 
 type Client struct {
-	SVIP              string
-	Endpoint          string
-	DefaultAPIPort    int
-	DefaultVolSize    int64 //bytes
-	DefaultAccountID  int64
-	DefaultTenantName string
-	Config            *Config
+	IOProtocol	string
+	Endpoint	string
+	ClusterName	string
+	TenantName	string
+	BucketName	string
+	ChunkSize	int64
+	Config		*Config
 }
 
-
 type Config struct {
+	Name		string // ndvol
+	NedgeHost	string // localhost
+	NedgePort	string // 8080
 	IOProtocol	string // NFS, iSCSI, NBD, S3
-	EndPoint	string // server:/export, IQN, devname, 
+	ClusterName	string
 	TenantName	string
-	AccessKey	string
-	SecretKey	string
-	MountPoint	string
+	BucketName	string
+	ChunkSize	int64
 }
 
 func ReadParseConfig(fname string) (Config, error) {
 	content, err := ioutil.ReadFile(fname)
 	if err != nil {
-		log.Fatal("Error processing config file: ", err)
+		log.Fatal(DN, "Error reading config file: ", fname, " error: ", err)
 	}
 	var conf Config
 	err = json.Unmarshal(content, &conf)
 	if err != nil {
-		log.Fatal("Error parsing config file: ", err)
+		log.Fatal(DN, "Error parsing config file: ", fname, " error: ", err)
 	}
 	return conf, nil
 }
@@ -44,21 +52,125 @@ func ReadParseConfig(fname string) (Config, error) {
 func ClientAlloc(configFile string) (c *Client, err error) {
 	conf, err := ReadParseConfig(configFile)
 	if err != nil {
-		log.Fatal("Error initializing client from Config file: ", configFile, "(", err, ")")
+		log.Fatal(DN, "Error initializing client from Config file: ", configFile, " error: ", err)
 	}
-
-	//TODO:
-	//DefaultApiPort
-	//DefaultAccountID
 	NdvolClient := &Client{
-		SVIP:		conf.IOProtocol,
-		Endpoint:	conf.EndPoint,
-		DefaultAPIPort:	8888,
-		DefaultAccountID:	9999,
-		DefaultTenantName: conf.TenantName,
-		Config:	&conf,
+		IOProtocol:		conf.IOProtocol,
+		Endpoint:		"http://" + conf.NedgeHost + "/" + string(conf.NedgePort) + "/",
+		ClusterName:		conf.ClusterName,
+		TenantName:		conf.TenantName,
+		BucketName:		conf.BucketName,
+		ChunkSize:		conf.ChunkSize,
+		Config:			&conf,
 	}
 
 	return NdvolClient, nil
 }
 
+func (c *Client) Request(method, endpoint string, data map[string]interface{}) (body []byte, err error) {
+	log.Debug("Issue request to Nexenta, endpoint: ", endpoint, " data: ", data, " method: ", method)
+	if c.Endpoint == "" {
+		log.Error("Endpoint is not set, unable to issue requests")
+		err = errors.New("Unable to issue json-rpc requests without specifying Endpoint")
+		return nil, err
+	}
+	datajson, err := json.Marshal(data)
+	if (err != nil) {
+		log.Error(err)
+	}
+
+	tr := &http.Transport{}
+	client := &http.Client{Transport: tr}
+	url := c.Endpoint + endpoint
+	req, err := http.NewRequest(method, url, nil)
+	if len(data) != 0 {
+		req, err = http.NewRequest(method, url, strings.NewReader(string(datajson)))
+	}
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Error("Error while handling request", err)
+		return nil, err
+	}
+	c.checkError(resp)
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if (err != nil) {
+		log.Error(err)
+	}
+	if (resp.StatusCode == 202) {
+		body, err = c.resend202(body)
+	}
+	return body, err
+}
+
+func (c *Client) resend202(body []byte) ([]byte, error) {
+	time.Sleep(1000 * time.Millisecond)
+	r := make(map[string][]map[string]string)
+	err := json.Unmarshal(body, &r)
+	if (err != nil) {
+		log.Error(err)
+	}
+
+	url := c.Endpoint + r["links"][0]["href"]
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Error("Error while handling request", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+	c.checkError(resp)
+
+	if resp.StatusCode == 202 {
+		body, err = c.resend202(body)
+	}
+	body, err = ioutil.ReadAll(resp.Body)
+	return body, err
+}
+
+func (c *Client) checkError(resp *http.Response) (bool) {
+	if resp.StatusCode > 399 {
+		body, err := ioutil.ReadAll(resp.Body)
+		log.Error(resp.StatusCode, string(body), err)
+		return true
+	}
+	return false
+}
+
+
+func (c *Client) CreateVolume(name string) (err error) {
+	log.Debug(DN, "Creating volume %s", name)
+	err = nil
+	return err
+}
+
+func (c *Client) DeleteVolume(name string) (err error) {
+	log.Debug(DN, "Deleting Volume ", name)
+	err = nil
+	return err
+}
+
+func (c *Client) MountVolume(name string) (err error) {
+	log.Debug(DN, "MountVolume ", name)
+	err = nil
+	return err
+}
+
+func (c *Client) UnmountVolume(name string) (err error) {
+	log.Debug(DN, "Unmounting Volume ", name)
+	err = nil
+	return err
+}
+
+func (c *Client) GetVolume(name string) (vname string, err error) {
+	log.Debug(DN, "GetVolume ", name)
+	vname = ""
+	err = nil
+	return vname, err
+}
+
+func (c *Client) ListVolumes() (vlist []string, err error) {
+	log.Debug(DN, "ListVolumes ")
+	err = nil
+	return vlist, err
+}
