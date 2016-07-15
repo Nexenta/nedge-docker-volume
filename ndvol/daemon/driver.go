03607d0edf2e95c59791f24abb072e2484c6d3e9
Nabin Acharya <nabin.acharya@nexenta.com>
2016-07-15 15:57:57 -0700

    cleanup structs and debug messages

--- ndvol/daemon/driver.go
+++ ndvol/daemon/driver.go
@@ -13,10 +13,7 @@ var (
 
 type NdvolDriver struct {
 	Scope		string
-	TenantID	int64
 	DefaultVolSz	int64
-	MountPoint	string
-	InitiatorIFace	string
 	Client		*ndvolapi.Client
 	Mutex		*sync.Mutex
 }
@@ -24,20 +21,12 @@ type NdvolDriver struct {
 func DriverAlloc(cfgFile string) NdvolDriver {
 
 	client, _ := ndvolapi.ClientAlloc(cfgFile)
-
-	mntPoint := "/foo"
-	initiator := "iscsiInterface"
-
 	d := NdvolDriver{
 		Scope:		"local",
-		TenantID:       1234,
 		DefaultVolSz:	1024,
 		Client:         client,
 		Mutex:          &sync.Mutex{},
-		MountPoint:     mntPoint,
-		InitiatorIFace: initiator,
 	}
-
 	return d
 }
 
@@ -49,53 +38,58 @@ func (d NdvolDriver) Capabilities(r volume.Request) volume.Response {
 
 func (d NdvolDriver) Create(r volume.Request) volume.Response {
 	log.Infof(DN, "Create volume %s on %s\n", r.Name, "nedge")
-
 	d.Mutex.Lock()
+	d.Client.CreateVolume(r.Name, d.DefaultVolSz);
 	defer d.Mutex.Unlock()
-
 	return volume.Response{}
 }
 
 
 func (d NdvolDriver) Get(r volume.Request) volume.Response {
-	log.Info(DN, "Get volume: ", r.Name)
-
+	log.Info(DN, "Get volume: ", r.Name, " MountID: ", r.MountID, " Options: ", r.Options)
+	/*
 	Name := "FooVol"
 	mntPoint := "/barmnt"
-
 	return volume.Response{Volume: &volume.Volume{Name: Name, Mountpoint: mntPoint}}
+	*/
+	d.Client.GetVolume(r.Name);
+	return volume.Response{}
 }
 
 func (d NdvolDriver) List(r volume.Request) volume.Response {
-	log.Info(DN, "Get volume: ", r.Name)
+	log.Info(DN, "List volume: ", r.Name, " MountID: ", r.MountID, " Options: ", r.Options)
 	var vols []*volume.Volume
+	d.Client.ListVolumes()
 	return volume.Response{Volumes: vols}
 }
 
 func (d NdvolDriver) Mount(r volume.Request) volume.Response {
+	log.Info(DN, "Mount volume: ", r.Name, " MountID: ", r.MountID, " Options: ", r.Options)
 	d.Mutex.Lock()
+	d.Client.MountVolume(r.Name)
 	defer d.Mutex.Unlock()
-	log.Infof(DN, "Mounting volume %s on %s\n", r.Name, "nedge")
-
-	return volume.Response{Mountpoint: "/" + r.Name}
+	return volume.Response{}
 }
 
 func (d NdvolDriver) Path(r volume.Request) volume.Response {
-	log.Info(DN, "Retrieve path info for volume: ", r.Name)
-	path := r.Name
-	log.Debug(DN, "Path reported as: ", path)
-	return volume.Response{Mountpoint: path}
+	log.Info(DN, "Path volume: ", r.Name, " MountID: ", r.MountID, " Options: ", r.Options)
+	d.Client.GetVolume(r.Name)
+	return volume.Response{}
 }
 
 func (d NdvolDriver) Remove(r volume.Request) volume.Response {
-
-	log.Info("DN, Remove/Delete Volume: ", r.Name)
-
+	log.Info(DN, "Remove volume: ", r.Name, " MountID: ", r.MountID, " Options: ", r.Options)
+	d.Mutex.Lock()
+	d.Client.DeleteVolume(r.Name)
+	defer d.Mutex.Unlock()
 	return volume.Response{}
 }
 
 func (d NdvolDriver) Unmount(r volume.Request) volume.Response {
-	log.Info("DN, Unmounting volume: ", r.Name)
+	log.Info(DN, "Unmount volume: ", r.Name, " MountID: ", r.MountID, " Options: ", r.Options)
+	d.Mutex.Lock()
+	d.Client.UnmountVolume(r.Name)
+	defer d.Mutex.Unlock()
 
 	return volume.Response{}
 }
--- ndvol/ndvolapi/ndvolapi.go
+++ ndvol/ndvolapi/ndvolapi.go
@@ -138,20 +138,41 @@ func (c *Client) checkError(resp *http.Response) (bool) {
 }
 
 
-func (c *Client) CreateVolume(name string) (err error) {
+func (c *Client) CreateVolume(name string, size int64) (err error) {
 	log.Debug(DN, "Creating volume %s", name)
+	path := c.ClusterName + "/" + c.TenantName + "/" + c.BucketName + "/" + name
+	data := map[string]interface{} {
+		"path": path,
+	}
+	data = make(map[string]interface{})
+	data["number"] = len(name)
+	data["volSizeMB"] = size >> 20
+	data["blockSize"] = c.ChunkSize
+	data["chunkSize"] = c.ChunkSize
+	data["objectPath"] = path
+	// _, err = c.Request("POST", c.EndPoint, data)
 	err = nil
 	return err
 }
 
 func (c *Client) DeleteVolume(name string) (err error) {
 	log.Debug(DN, "Deleting Volume ", name)
-	err = nil
+	path := c.ClusterName + "/" + c.TenantName + "/" + c.BucketName + "/" + name
+	data := map[string]interface{} {
+		"path": path,
+	}
+	data = make(map[string]interface{})
+	data["number"] = len(name)
+	data["volSizeMB"] = c.ChunkSize
+	data["blockSize"] = c.ChunkSize
+	data["chunkSize"] = c.ChunkSize
+	data["objectPath"] = path
+	// _, err = c.Request("DELETE", c.EndPoint, data)
 	return err
 }
 
 func (c *Client) MountVolume(name string) (err error) {
-	log.Debug(DN, "MountVolume ", name)
+	log.Debug(DN, "Mounting Volume ", name)
 	err = nil
 	return err
 }
